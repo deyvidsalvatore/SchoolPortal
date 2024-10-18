500a3eff90f5cb67ede3501a17e92c4efe78073f
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 15:22:25 -0300

    chore: Changing toggle menu icons

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const materialModules = [
   MatCardModule,
@@ -13,7 +14,8 @@ const materialModules = [
   MatSidenavModule,
   MatToolbarModule,
   MatListModule,
-  MatIconModule
+  MatIconModule,
+  MatTooltipModule
 ];
 
 @NgModule({
