f9a63fd48cc55c7fd0ff5dd35a52c2e92fa13ec4
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 09:19:46 -0300

    feat: First Navbar

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialModules = [
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatIconModule
 ];
 
 @NgModule({
