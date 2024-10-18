7262d3fd906e908e7280739db0cf1d20f0baecb5
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 14:02:52 -0300

    chore: Changes on App Routing, Template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,41 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/pages/users/users.module').then((m) => m.UsersModule),
   },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/pages/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./modules/pages/courses/courses.module').then(
+        (m) => m.CoursesModule
+      ),
+  },
+  {
+    path: 'teachers',
+    loadChildren: () =>
+      import('./modules/pages/teachers/teachers.module').then(
+        (m) => m.TeachersModule
+      ),
+  },
+  {
+    path: 'students',
+    loadChildren: () =>
+      import('./modules/pages/students/students.module').then(
+        (m) => m.StudentsModule
+      ),
+  },
+  {
+    path: 'support',
+    loadChildren: () =>
+      import('./modules/pages/support/support.module').then(
+        (m) => m.SupportModule
+      ),
+  },
 ];
 
 @NgModule({
