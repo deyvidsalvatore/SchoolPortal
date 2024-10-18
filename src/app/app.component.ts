40dc6dec0c00ee532b7e2b82305b089a06a485e6
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 15:49:55 -0300

    chore: Customizing Toolbars and Updating Layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { AfterContentInit, Component, OnInit } from '@angular/core';
-import { fromEvent, map } from 'rxjs';
+import { AfterContentInit, Component, inject, OnInit } from '@angular/core';
+import { filter, fromEvent, map } from 'rxjs';
 import { MenuItem } from './shared/models/menu-item.interface';
 import { menuItems } from './shared/models/menu';
+import { NavigationEnd, Router } from '@angular/router';
 
 export const SCROLL_CONTAINER = 'mat-sidenav-content';
 export const TEXT_LIMIT = 50;
@@ -19,19 +20,32 @@ export class AppComponent implements OnInit, AfterContentInit {
   public popText = false;
   public applyShadow = false;
   public items_menu: MenuItem[] = menuItems;
+  private breakpointObserver: BreakpointObserver;
+  private route: Router;
+  public menuName = '';
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor() {
+    this.breakpointObserver = inject(BreakpointObserver);
+    this.route = inject(Router);
+  }
 
   ngOnInit(): void {
     const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
 
     fromEvent(content, 'scroll')
-      .pipe(
-        map(() => content.scrollTop)
-      )
-      .subscribe({
-        next: (value: number) => this.determineHeader(value)
-      });
+      .pipe(map(() => content.scrollTop))
+      .subscribe((value: number) => this.determineHeader(value))
+
+    this.route.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => event as NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      let moduleName = event.url.split('/')[1]
+
+      this.menuName = this.items_menu.filter(
+        (item: MenuItem) => item.link == `/${moduleName}`
+      )[0].label;
+    });
   }
 
   determineHeader(scrollTop: number) {
