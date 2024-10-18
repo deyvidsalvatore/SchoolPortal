1f9f6e251a6b3edd6d0cfd9dc9f570c8237112c5
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 10:41:14 -0300

    feat: Generating User components page

--- src/app/modules/pages/users/users.module.ts
+++ src/app/modules/pages/users/users.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
-
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyConquestsComponent } from './my-conquests/my-conquests.component';
+import { MyDisciplinesComponent } from './my-disciplines/my-disciplines.component';
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    MyProfileComponent,
+    MyConquestsComponent,
+    MyDisciplinesComponent
   ],
   imports: [
     CommonModule,
     UsersRoutingModule
+  ],
+  exports: [
+
   ]
 })
 export class UsersModule { }
