b4222e2e65ea6f5dcc04744e6fb53878a0626c28
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 22:52:09 -0300

    feat: Course List Components and Query methods

--- src/app/modules/pages/courses/courses.module.ts
+++ src/app/modules/pages/courses/courses.module.ts
@@ -5,16 +5,22 @@ import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
 import { CoursesMaterialModule } from '@app/shared/material/courses-material.module';
 import { SharedModule } from '@app/shared/shared.module';
+import { CourseListComponent } from './course-list/course-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseListComponent
   ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
+    ReactiveFormsModule,
     CoursesMaterialModule,
+    FlexLayoutModule,
     SharedModule
   ]
 })
--- src/app/shared/material/courses-material.module.ts
+++ src/app/shared/material/courses-material.module.ts
@@ -1,17 +1,56 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 const coursesMaterialModules = [
   MatIconModule,
-  MatButtonModule
+  MatButtonModule,
+  MatDividerModule,
+  MatCardModule,
+  MatInputModule,
+  MatSelectModule,
+  MatPaginatorModule,
+  MatFormFieldModule,
+  MatProgressBarModule,
+  MatSnackBarModule
 ];
+
+const customPaginator = () => {
+  const customPaginatorIntl = new MatPaginatorIntl();
+  customPaginatorIntl.itemsPerPageLabel = '';
+  customPaginatorIntl.nextPageLabel = 'próxima';
+  customPaginatorIntl.previousPageLabel = 'anterior';
+  customPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0 || pageSize === 0) {
+      return `0 de ${length }`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return `${startIndex + 1} - ${endIndex} de ${length}`;
+  };
+  return customPaginatorIntl;
+}
+
 
 
 @NgModule({
   declarations: [],
   imports: [coursesMaterialModules],
-  exports: [coursesMaterialModules]
+  exports: [coursesMaterialModules],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: customPaginator() }
+  ]
 })
 export class CoursesMaterialModule { }
+
