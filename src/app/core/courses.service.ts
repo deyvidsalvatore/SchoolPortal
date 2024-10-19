c8e97e8a396e6001fb2c3a4f24e52deb1477732f
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-10-18 22:01:33 -0300

    feat: Environment variables

---
+++ src/app/core/courses.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+const baseUrl: string = `${environment.baseUrl}/courses`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+  
+
+}
